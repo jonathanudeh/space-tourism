bbc622c6bf582c2599fd5aedcb8aedb04765de6f
Jonathan Udeh <jonathanudeh06@gmail.com>
2025-10-21 16:24:46 +0100

    Styling the crew page

--- src/features/crew/CrewPage.jsx
+++ src/features/crew/CrewPage.jsx
@@ -4,23 +4,29 @@ import data from "../../data.json";
 function CrewPage() {
   const crew = data.crew;
   const [current, setCurrent] = useState(crew[0]);
-  // console.log(current);
 
   return (
     <div className="h-dvh bg-[url('/assets/crew/background-crew-mobile.jpg')] bg-cover bg-center bg-no-repeat sm:bg-[url('/assets/crew/background-crew-tablet.jpg')] md:bg-[url('/assets/crew/background-crew-desktop.jpg')]">
+      {/*  */}
+
+      {/*  */}
       <div className="flex h-full flex-col p-6 md:flex-row">
-        <div className="flex h-1/2 flex-col items-center justify-around px-6 py-4 text-white md:h-full md:items-start">
-          <p className="space-x-2 text-xl font-medium text-blue-100/80 uppercase md:m-6 md:self-start md:text-2xl">
+        <div className="flex h-1/2 flex-col items-center justify-center space-y-6 px-6 py-4 text-white md:h-full md:items-start">
+          <p className="space-x-2 text-xl font-medium text-blue-100/80 uppercase md:self-start md:text-2xl">
             <span className="font-bold text-blue-100/50">02</span>
             <span>Meet your crew</span>
           </p>
 
-          <div>
-            <p className="text-xl font-semibold text-blue-100/80 uppercase">
+          <div className="space-y-5 text-center md:text-start">
+            <p className="text-xl font-semibold text-blue-100/70 uppercase">
               {current.role}
             </p>
-            <h1 className="text-2xl font-bold uppercase">{current.name}</h1>
-            <p className="max-w-sm md:max-w-lg lg:max-w-2xl">{current.bio}</p>
+            <h1 className="text-3xl font-bold text-blue-50 uppercase md:text-5xl">
+              {current.name}
+            </h1>
+            <p className="max-w-sm text-blue-100/80 md:max-w-md md:pr-3 lg:max-w-lg">
+              {current.bio}
+            </p>
           </div>
 
           <ul className="flex gap-4">
@@ -28,18 +34,18 @@ function CrewPage() {
               <li
                 key={member.role}
                 onClick={() => setCurrent(member)}
-                className="h-3 w-3 cursor-pointer rounded-full bg-amber-300"
+                className={`h-3 w-3 cursor-pointer rounded-full bg-blue-100/30 hover:bg-blue-50 ${current.role === member.role ? "bg-white" : ""}`}
               ></li>
             ))}
           </ul>
         </div>
 
         {/*  */}
-        <div className="flex h-1/2 basis-1/2 flex-col items-center justify-center md:h-full">
+        <div className="flex h-1/2 basis-1/2 flex-col items-center justify-end md:h-full md:items-end">
           <img
-            className="mask-gradient h-40 w-40 md:h-120 md:w-120"
+            className="mask-gradient h-60 w-60 md:h-120 md:w-120"
             src={current.images.webp}
-            alt={`The image of ${current.name}`}
+            alt={`The image of the ${current.role}`}
           />
         </div>
       </div>
--- src/ui/MobileNav.jsx
+++ src/ui/MobileNav.jsx
@@ -1,19 +1,27 @@
 import { NavLink } from "react-router-dom";
 
 function MobileNav({ links, navOpen, onNavOpen }) {
+  function handleNavClose() {
+    onNavOpen(false);
+  }
+
   return (
     <div
-      className={`fixed top-0 right-0 flex h-dvh w-50 flex-col gap-15 bg-transparent pt-5 pl-5 backdrop-blur-lg transition-all duration-500 md:hidden ${navOpen ? "translate-x-0 " : "translate-x-full"}`}
+      className={`fixed top-0 right-0 z-50 flex h-dvh w-50 flex-col gap-15 bg-transparent pt-5 pl-5 backdrop-blur-lg transition-all duration-500 md:hidden ${navOpen ? "translate-x-0 " : "translate-x-full"}`}
     >
       {/* close icon */}
-      <button className="self-end p-4" onClick={() => onNavOpen(false)}>
+      <button className="self-end p-4" onClick={handleNavClose}>
         <img src="/assets/shared/icon-close.svg" alt="Close hamburger sign" />
       </button>
 
       {/* navs */}
       <ul className="w-full gap-5 space-y-8">
         {links.map((link) => (
-          <li key={link.to} className="w-full text-start">
+          <li
+            key={link.to}
+            className="w-full text-start"
+            onClick={handleNavClose}
+          >
             <NavLink
               to={link.to}
               className={({ isActive }) =>
