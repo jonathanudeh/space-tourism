5de1fbb107196b3f3d410e5f376d06150bf4ca16
Jonathan Udeh <jonathanudeh06@gmail.com>
2025-10-23 17:19:31 +0100

    added framer motion for animation

--- src/features/crew/CrewPage.jsx
+++ src/features/crew/CrewPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import data from "../../data.json";
+import { AnimatePresence, motion } from "framer-motion";
 
 function CrewPage() {
   const crew = data.crew;
@@ -10,43 +11,60 @@ function CrewPage() {
       {/*  */}
 
       {/*  */}
-      <div className="flex h-full flex-col p-6 md:flex-row">
-        <div className="flex h-1/2 flex-col items-center justify-center space-y-6 px-6 py-4 text-white md:h-full md:items-start">
+      <div className="flex h-full flex-col items-center justify-end gap-5 px-6 py-3 md:flex-row md:justify-around md:gap-8">
+        {/*  */}
+        <div className="flex flex-col items-center justify-center gap-2 px-6 text-white md:h-full md:items-start md:justify-between md:py-20">
           <p className="space-x-2 text-xl font-medium text-blue-100/80 uppercase md:self-start md:text-2xl">
             <span className="font-bold text-blue-100/50">02</span>
             <span>Meet your crew</span>
           </p>
 
-          <div className="space-y-5 text-center md:text-start">
-            <p className="text-xl font-semibold text-blue-100/70 uppercase">
-              {current.role}
-            </p>
-            <h1 className="text-3xl font-bold text-blue-50 uppercase md:text-5xl">
-              {current.name}
-            </h1>
-            <p className="max-w-sm text-blue-100/80 md:max-w-md md:pr-3 lg:max-w-lg">
-              {current.bio}
-            </p>
-          </div>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={current.name}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="flex min-h-[280px] flex-col items-center justify-center py-4 text-center md:items-start md:text-start"
+            >
+              <p className="text-xl font-semibold text-blue-100/70 uppercase md:mb-3">
+                {current.role}
+              </p>
+              <h1 className="mb-6 text-3xl font-bold text-blue-50 uppercase md:text-5xl">
+                {current.name}
+              </h1>
+              <p className="max-w-sm text-base text-blue-100/80 md:max-w-md md:pr-3 md:text-xl lg:max-w-lg">
+                {current.bio}
+              </p>
+            </motion.div>
+          </AnimatePresence>
 
           <ul className="flex gap-4">
             {crew.map((member) => (
               <li
                 key={member.role}
                 onClick={() => setCurrent(member)}
-                className={`h-3 w-3 cursor-pointer rounded-full hover:bg-blue-50 ${current.role === member.role ? "bg-white" : "bg-blue-100/30"}`}
+                className={`h-4 w-4 cursor-pointer rounded-full hover:bg-blue-50 md:h-6 md:w-6 ${current.role === member.role ? "bg-white" : "bg-blue-100/30"}`}
               ></li>
             ))}
           </ul>
         </div>
 
         {/*  */}
-        <div className="flex h-1/2 basis-1/2 flex-col items-center justify-end md:h-full md:items-end">
-          <img
-            className="mask-gradient h-60 w-60 md:h-120 md:w-120"
-            src={current.images.webp}
-            alt={`The image of the ${current.role}`}
-          />
+        <div className="flex flex-col items-center justify-end md:h-full md:items-end">
+          <AnimatePresence mode="wait">
+            <motion.img
+              key={current.name}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="mask-gradient h-60 w-60 md:h-120 md:w-120"
+              src={current.images.webp}
+              alt={`The image of the ${current.role}`}
+            />
+          </AnimatePresence>
         </div>
       </div>
     </div>
--- src/features/destination/DestinationPage.jsx
+++ src/features/destination/DestinationPage.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import data from "../../data.json";
+import { AnimatePresence, motion } from "framer-motion";
 
 function DestinationPage() {
   const destination = data.destinations;
   const [current, setCurrent] = useState(destination[0]);
+  console.log(typeof motion);
 
   return (
     <div className="h-dvh bg-[url('/assets/destination/background-destination-mobile.jpg')] bg-cover bg-center bg-no-repeat sm:bg-[url('/assets/destination/background-destination-tablet.jpg')] md:bg-[url('/assets/destination/background-destination-desktop.jpg')]">
-      <div className="flex h-full flex-col items-center justify-center gap-1 md:flex-row">
-        <div className="flex basis-1/2 flex-col items-center justify-center">
+      {/*  */}
+      <div className="flex h-full flex-col items-center justify-center gap-1 px-2 py-6 md:flex-row md:px-5">
+        {/*  */}
+        <div className="flex basis-1/2 flex-col items-center justify-end py-5">
           <p className="mb-2 space-x-2 text-xl font-medium text-blue-100/80 uppercase md:m-6 md:self-start md:text-2xl">
             <span className="font-bold text-blue-100/50">01</span>
             <span>pick your destination</span>
           </p>
-          <img
-            className="h-40 w-40 md:h-120 md:w-120"
-            src={current.images.png}
-            alt={`The image of ${current.name}`}
-          />
+          <AnimatePresence mode="wait">
+            <motion.img
+              key={current.name}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="h-40 w-40 md:h-120 md:w-120"
+              src={current.images.png}
+              alt={`The image of ${current.name}`}
+            />
+          </AnimatePresence>
         </div>
 
         {/*  */}
@@ -38,31 +49,40 @@ function DestinationPage() {
             ))}
           </div>
 
-          <div className="text-white md:text-start">
-            <div className="mb-2 text-5xl font-medium uppercase md:mb-8 md:text-7xl">
-              {current.name}
-            </div>
-            <div className="mb-2 border-b border-blue-50/40 py-2 pb-4 text-sm text-blue-100/80 md:mb-8 md:text-lg">
-              {current.description}
-            </div>
-            <div className="flex flex-col items-center justify-around uppercase sm:flex-row md:justify-start md:gap-10">
-              <div className="mb-2 md:mb-0">
-                <span className="text-sm font-light text-blue-100/70">
-                  AVG. Distance
-                </span>
-                <br />
-                <span className="text-2xl">{current.distance}</span>
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={current.name}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="text-white md:text-start"
+            >
+              <div className="mb-2 text-5xl font-medium uppercase md:mb-8 md:text-7xl">
+                {current.name}
               </div>
+              <div className="mb-2 min-h-[110px] border-b border-blue-50/40 py-2 pb-4 text-sm text-blue-100/80 md:mb-8 md:min-h-[140px] md:text-lg">
+                {current.description}
+              </div>
+              <div className="flex flex-col items-center justify-around uppercase sm:flex-row md:justify-start md:gap-10">
+                <div className="mb-2 md:mb-0">
+                  <span className="text-sm font-light text-blue-100/70">
+                    AVG. Distance
+                  </span>
+                  <br />
+                  <span className="text-2xl">{current.distance}</span>
+                </div>
 
-              <div className="text-sm font-light">
-                <span className="font-light text-blue-100/70">
-                  Est. Travel Time
-                </span>
-                <br />
-                <span className="text-2xl">{current.travel}</span>
+                <div className="text-sm font-light">
+                  <span className="font-light text-blue-100/70">
+                    Est. Travel Time
+                  </span>
+                  <br />
+                  <span className="text-2xl">{current.travel}</span>
+                </div>
               </div>
-            </div>
-          </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
     </div>
